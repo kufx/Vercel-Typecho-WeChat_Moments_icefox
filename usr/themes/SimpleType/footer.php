02d7d85bfcbfca3a5d5e166183d73efd90bee966
kufx <144138627+kufx@users.noreply.github.com>
2025-01-29 22:29:08 +0800

    Delete usr/themes/SimpleType directory

--- usr/themes/SimpleType/footer.php
+++
@@ -1,5 +0,0 @@
-	<div class="foot">Copyright © 2015 <a href="<?php $this->options->siteurl(); ?>"><?php $this->options->title(); ?></a> 版权所有.网站运行：<?php getBuildTime(); ?></div><!-- end #footer -->
-</div>
-<?php $this->footer(); ?>
-</body>
-</html>
